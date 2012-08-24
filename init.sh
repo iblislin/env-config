c435f6f00ba9a56894beb4fd80e80de639d1b3cd
iblis17 <iblis@hs.ntnu.edu.tw>
2012-08-24 12:17:59 +0800

    use symbolic link for .vim.

--- init.sh
+++ init.sh
@@ -8,16 +8,16 @@ LN='ln -s'
 ckfile()
 {
 	printf "$1 : "
-		if [ ! -O $BASE/$1 ] 
+		if [ ! -O $HOME/$1 ] 
 		then
 			printf 'Wrong Owner!\n'
-		elif [ ! -w $BASE/$1 ]
+		elif [ ! -w $HOME/$1 ]
 		then
 			printf 'Permission Deny!\n'
-		elif [ ! -L $BASE/$1 ]
+		elif [ ! -L $HOME/$1 ]
 		then
-			rm -rf ~/$1
-			$LN $BASE/$1 ~/$1
+			rm -rf $HOME/$1
+			$LN $BASE/$1 $HOME/$1
 			printf 'Done.\n'
 		else
 			printf	'Link existed.\n'
@@ -27,12 +27,11 @@ ckfile()
 vim()
 {
 	ckfile ".vimrc"
-	rm -rf ~/.vim
-	mkdir -p ~/.vim/tmp ~/.vim/backup
-	for i in `ls vim`
-	do
-		$LN $BASE/vim/$i ~/.vim
-	done
+	if [ -d $HOME/.vim ]
+	then
+		rm -rf $HOME/.vim
+		$LN $BASE/vim $HOME/.vim
+	fi
 	sh ./ctags.sh
 }
 
