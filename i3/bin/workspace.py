410840d605eb5164b3f9f9b7f25faca68b2e45ec
Iblis Lin <e196819@hotmail.com>
2015-12-22 01:07:13 +0800

    [i3] script for rename workspaces

--- i3/bin/workspace.py
+++ i3/bin/workspace.py
@@ -4,24 +4,57 @@ import i3ipc
 import sys
 
 
+OUTPUT = {
+    'DVI-I-1': 0,
+    'VGA-1':   10,
+}
+
+DVI = 'DVI-I-1'
+VGA = 'VGA-1'
+
+WS_MAP = ['', 'α', 'β', 'ξ', 'δ', 'ε', 'φ', 'γ', 'θ']
+
+
 def current_workspace(workspaces):
     for ws in workspaces:
         if ws.focused is True:
             return ws
 
 
+def class_ws(workspaces):
+    ret = {
+        DVI: [],
+        VGA: [],
+    }
+
+    for ws in workspaces:
+        if ws.output == DVI:
+            ret[DVI].append(ws)
+        elif ws.output == VGA:
+            ret[VGA].append(ws)
+
+    return ret
+
+
+def on_ws_focus(i3, e):
+    workspaces = i3.get_workspaces()
+    cur = current_workspace(workspaces)
+    output = cur.output
+    ws_ls = class_ws(workspaces)
+
+    num = ws_ls[output].index(cur) + 1 + OUTPUT[output]
+
+    char = WS_MAP[num - OUTPUT[output]]
+    print('rename to {}:{}'.format(num, char))
+    i3.command('rename workspace to {}:{}'.format(num, char))
+
+
 def main():
     i3 = i3ipc.Connection()
 
-    workspaces = i3.get_workspaces()
-    num = len(workspaces)
-    current = current_workspace(workspaces).num
+    i3.on('workspace::focus', on_ws_focus)
 
-    to = current + int(sys.argv[1])
-    if to < 1:
-        to = workspaces[to-1].num
-    print(to)
-    i3.command("workspace number {}".format(to))
+    i3.main()
 
 
 if __name__ == '__main__':
