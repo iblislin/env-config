1498f8e9ce4eb43bd255932ebd8fc317d0a09da9
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-13 21:18:41 +0800

    emacs: scrolling

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -69,3 +69,17 @@
 (moe-dark)
 
 (global-set-key (kbd "C-z") 'set-mark-command)
+
+(defun window-half-height ()
+  (max 1 (/ (1- (window-height (selected-window))) 2)))
+
+(defun scroll-up-half ()
+  (interactive)
+  (scroll-up (window-half-height)))
+
+(defun scroll-down-half ()
+  (interactive)
+  (scroll-down (window-half-height)))
+
+(global-set-key (kbd "C-d") 'scroll-up-half)
+(global-set-key (kbd "C-f") 'scroll-down-half)
