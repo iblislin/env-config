51653c91acdd67da86f042d9a32d33d48630ec90
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-23 15:21:06 +0800

    backup dir

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -123,3 +123,13 @@
 
 ;; line number
 (global-linum-mode t)
+
+;;change backup dir
+(setq
+  backup-by-copying t      ; don't clobber symlinks
+  backup-directory-alist
+  '(("." . "~/.emacs.d/backup"))    ; don't litter my fs tree
+  delete-old-versions t
+  kept-new-versions 9
+  kept-old-versions 2
+  version-control t)       ; use versioned backups
