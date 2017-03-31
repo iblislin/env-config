16832ce54f03928b4a68f36368b8410e542d59d9
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-04-01 01:26:48 +0800

    awesome4: indent

--- awesome4/rc.lua
+++ awesome4/rc.lua
@@ -127,46 +127,47 @@ mytextclock = wibox.widget.textclock()
 
 -- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(
-                    awful.button({ }, 1, function(t) t:view_only() end),
-                    awful.button({ modkey }, 1, function(t)
-                                              if client.focus then
-                                                  client.focus:move_to_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
-                    awful.button({ modkey }, 3, function(t)
-                                              if client.focus then
-                                                  client.focus:toggle_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 4, function(t) awful.tag.viewprev(t.screen) end),
-                    awful.button({ }, 5, function(t) awful.tag.viewnext(t.screen) end)
-                )
+    awful.button({ }, 1, function(t) t:view_only() end),
+    awful.button({ modkey }, 1, function(t)
+                                    if client.focus then
+                                        client.focus:move_to_tag(t)
+                                    end
+                                end),
+    awful.button({ }, 3, awful.tag.viewtoggle),
+    awful.button({ modkey }, 3, function(t)
+                                    if client.focus then
+                                        client.focus:toggle_tag(t)
+                                    end
+                                end),
+    awful.button({ }, 4, function(t) awful.tag.viewprev(t.screen) end),
+    awful.button({ }, 5, function(t) awful.tag.viewnext(t.screen) end)
+)
 
 local tasklist_buttons = gears.table.join(
-                     awful.button({ }, 1, function (c)
-                                              if c == client.focus then
-                                                  c.minimized = true
-                                              else
-                                                  -- Without this, the following
-                                                  -- :isvisible() makes no sense
-                                                  c.minimized = false
-                                                  if not c:isvisible() and c.first_tag then
-                                                      c.first_tag:view_only()
-                                                  end
-                                                  -- This will also un-minimize
-                                                  -- the client, if needed
-                                                  client.focus = c
-                                                  c:raise()
-                                              end
-                                          end),
-                     awful.button({ }, 3, client_menu_toggle_fn()),
-                     awful.button({ }, 4, function ()
-                                              awful.client.focus.byidx(1)
-                                          end),
-                     awful.button({ }, 5, function ()
-                                              awful.client.focus.byidx(-1)
-                                          end))
+    awful.button({ }, 1, function (c)
+                            if c == client.focus then
+                                c.minimized = true
+                            else
+                                -- Without this, the following
+                                -- :isvisible() makes no sense
+                                c.minimized = false
+                                if not c:isvisible() and c.first_tag then
+                                    c.first_tag:view_only()
+                                end
+                                -- This will also un-minimize
+                                -- the client, if needed
+                                client.focus = c
+                                c:raise()
+                            end
+                         end),
+    awful.button({ }, 3, client_menu_toggle_fn()),
+    awful.button({ }, 4, function ()
+                            awful.client.focus.byidx(1)
+                         end),
+    awful.button({ }, 5, function ()
+                            awful.client.focus.byidx(-1)
+                         end)
+)
 
 local function set_wallpaper(s)
     -- Wallpaper
