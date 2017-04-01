b8fe1397ea9e06bbbaa4d86fd8fff09719a36a5b
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-04-01 17:35:55 +0800

    init_arch: mosh

--- scripts/init_arch.sh
+++ scripts/init_arch.sh
@@ -3,7 +3,7 @@ PACMAN='sudo pacman -S'
 # upgrade system first
 sudo pacman -Syu
 
-$PACMAN git wget curl net-tools openssh iotop rsync
+$PACMAN git wget curl net-tools openssh iotop rsync mosh
 
 # python
 $PACMAN python python-pip
