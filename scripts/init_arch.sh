3d164193810e7d7317212a8f4a564e2a66e9dfe0
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-04-01 17:34:56 +0800

    init_arch rsync

--- scripts/init_arch.sh
+++ scripts/init_arch.sh
@@ -3,7 +3,7 @@ PACMAN='sudo pacman -S'
 # upgrade system first
 sudo pacman -Syu
 
-$PACMAN git wget curl net-tools openssh iotop
+$PACMAN git wget curl net-tools openssh iotop rsync
 
 # python
 $PACMAN python python-pip
