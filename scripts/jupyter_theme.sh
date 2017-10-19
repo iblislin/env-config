8dabed99dc8ea2c462db23d5d48c8f80370ecc74
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-10-19 16:01:17 +0800

    jt

---
+++ scripts/jupyter_theme.sh
@@ -0,0 +1,16 @@
+
+# pip install -U jupyterthemes
+
+jt\
+    -f firacode \
+    -t monokai \
+    -fs 14 \
+    -cursc x \
+    -cursw 10 \
+    -T \
+    -altp \
+    -ofs 13 \
+    -N \
+    -cellw 100% \
+    -altout \
+    -lineh 120
