db0d7926fb7008888692d6ba46898d25d9acd33b
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-01-13 18:53:09 +0800

    script: init, htop ntp

--- scripts/init_arch.sh
+++ scripts/init_arch.sh
@@ -3,8 +3,8 @@ PACMAN='sudo pacman -S'
 # upgrade system first
 sudo pacman -Syu
 
-$PACMAN git wget curl net-tools iotop rsync nload unzip tmux \
-        openssh mosh \
+$PACMAN git wget curl net-tools iotop rsync nload unzip tmux htop \
+        openssh mosh ntp \
         feh imagemagick \
         cmake lm_sensors \
         smartmontools \
