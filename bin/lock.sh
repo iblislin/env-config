7e26d111895e350f2714137b24317a73f78dd69b
Iblis Lin <iblislin@gmail.com>
2023-06-16 19:24:39 +0800

    update lock

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,3 +1,6 @@
-scrot -o /tmp/screenshot.png
-convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-i3lock -i /tmp/screenshotblur.png
+# scrot -o /tmp/screenshot.png
+# convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+# i3lock -i /tmp/screenshotblur.png
+
+# betterlockscreen -u ~/Pictures/screenlock.jpg
+betterlockscreen -l dim --off 60
